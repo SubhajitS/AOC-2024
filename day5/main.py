8934bee6cc7e053048fce45c021bd7acd33298ca
Subhajit Saha <subhajit.saha@equalexperts.com>
2024-12-05 12:23:19 +0530

    Day 5 part 1

---
+++ day5/main.py
@@ -0,0 +1,38 @@
+orders={}
+updates=[]
+
+def readinput():
+    inputfile = open("./input.txt", "r")
+    ordersFinished = False
+    for row in inputfile.readlines():
+        if row == "\n":
+            ordersFinished = True
+            continue
+        if ordersFinished:
+            updates.append([int(num) for num in row.rstrip("\n").split(",")])
+        else:
+            order = row.rstrip("\n").split('|')
+            key = int(order[0])
+            value = int(order[1])
+            if key in orders:
+                newvalue = list(orders[key])
+                newvalue.append(value)
+                orders[key] = newvalue
+            else:
+                orders[key] = [value]
+
+def isvalid(update: list) -> bool:
+    for i in range(len(update)):
+        for j in range(i+1, len(update)):
+            if update[j] in orders:
+                if update[i] in orders[update[j]]:
+                    return False
+    return True
+
+if __name__ == "__main__":
+    readinput()
+    sum = 0
+    for update in updates:
+        if isvalid(update):
+            sum += update[int(len(update)/2)]
+    print(sum)
